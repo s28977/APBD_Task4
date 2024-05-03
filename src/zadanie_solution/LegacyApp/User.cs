d2436b70fa672224b80035ff82d04769db373475
s28977 <s28977@pjwstk.edu.pl>
2024-05-03 21:27:11 +0200

    Moved user fields validation from UserService to User

--- src/zadanie_solution/LegacyApp/User.cs
+++ src/zadanie_solution/LegacyApp/User.cs
@@ -4,11 +4,64 @@ namespace LegacyApp
 {
     public class User
     {
-        public object Client { get; internal set; }
-        public DateTime DateOfBirth { get; internal set; }
-        public string EmailAddress { get; internal set; }
-        public string FirstName { get; internal set; }
-        public string LastName { get; internal set; }
+        private string _firstName;
+        private string _lastName;
+        private string _emailAddress;
+        private DateTime _dateOfBirth;
+        public Client Client { get; internal set; }
+
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            internal set
+            {
+                var now = DateTime.Now;
+                var age = now.Year - _dateOfBirth.Year;
+                if (now.Month < _dateOfBirth.Month || (now.Month == _dateOfBirth.Month && now.Day < _dateOfBirth.Day)) age--;
+
+                if (age < 21)
+                {
+                    throw new ArgumentException("Age of user must be at least 21 years old.");
+                }
+            }
+        }
+
+        public string EmailAddress
+        {
+            get => _emailAddress;
+            internal set
+            {
+                if (!_emailAddress.Contains("@") && !_emailAddress.Contains("."))
+                {
+                    throw new ArgumentException("Email address of user must contain character  '.' and '@'.");
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            internal set
+            {
+                if (string.IsNullOrEmpty(_firstName))
+                {
+                    throw new ArgumentException("First name of user can't be null or empty.");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            internal set
+            {
+                if (string.IsNullOrEmpty(_lastName))
+                {
+                    throw new ArgumentException("Last name of user can't be null or empty.");
+                }
+            }
+        }
+
         public bool HasCreditLimit { get; internal set; }
         public int CreditLimit { get; internal set; }
     }
--- src/zadanie_solution/LegacyApp/UserService.cs
+++ src/zadanie_solution/LegacyApp/UserService.cs
@@ -2,29 +2,11 @@
 
 namespace LegacyApp
 {
+    //adds user to user db
     public class UserService
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
-            {
-                return false;
-            }
-
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
-
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
 
